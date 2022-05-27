a475382ce6aac73a9f4836fc456c573038d129db
RenaWagner <rena.suzuki.wagner@gmail.com>
2022-05-27 15:34:37 +0200

    Prevent SQL injection

--- details.php
+++ details.php
@@ -8,13 +8,13 @@ $database = "exercise_data";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 $ID = mysqli_real_escape_string($conn, $_GET['ID']);
-$sql = "SELECT articles.title, images.path,articles.uuid FROM articles INNER JOIN images ON articles.headerImage_uuid=images.uuid WHERE articles.id=$ID" ;
+
+$sql = "SELECT articles.title, images.path,articles.uuid FROM articles LEFT JOIN images ON articles.headerImage_uuid=images.uuid WHERE articles.id=$ID" ;
 $result = mysqli_query($conn, $sql);
 $articles = mysqli_fetch_all($result, MYSQLI_ASSOC);
-print_r($articles);
 
 $articleUuid =$articles[0]['uuid']; 
-$sqlParagraph = "SELECT * FROM paragraphs INNER JOIN images ON paragraphs.image_uuid=images.uuid WHERE paragraphs.article_id='$articleUuid'" ;
+$sqlParagraph = "SELECT * FROM paragraphs LEFT JOIN images ON paragraphs.image_uuid=images.uuid WHERE paragraphs.article_id='$articleUuid'" ;
 $resultParagraph = mysqli_query($conn, $sqlParagraph);
 $paragraphs = mysqli_fetch_all($resultParagraph, MYSQLI_ASSOC);
 }
@@ -32,16 +32,20 @@ foreach($articles as $article => $value) {
   $title = $value['title'];
   $headerImageUrl =  $value['path'];
   echo "<h1>$title</h1>";
-echo "<img src='$headerImageUrl' alt='headerImage' class='headerImage'/>";
+  if ($headerImageUrl){
+    echo "<img src='$headerImageUrl' alt='headerImage' class='headerImage'/>";
+  }
 }
 
 foreach($paragraphs as $paragraph => $value){
   $title = $value['title'];
    $text = $value['text'];
-  $headerImageUrl =  $value['path'];
+  $paragraphImageUrl =  $value['path'];
   echo "<h2>$title</h2>";
   echo "<p>$text</p>";
-  echo "<img src='$headerImageUrl' alt='headerImage' class='headerImage'/>";
+   if ($paragraphImageUrl){
+    echo "<img src='$paragraphImageUrl' alt='paragraphImageUrl' class='headerImage'/>";
+  }
 }
 ?>  
 
--- form.php
+++ form.php
@@ -1,30 +1,29 @@
 <?php 
-// action="index.php" 
 require 'vendor/autoload.php';
 use Ramsey\Uuid\Uuid;
 
-
-// printf("Your UUID is: %s", $myuuid->toString());
-
 if ($_POST)
 {
-  $title = $_POST["title"];
-  $text = $_POST["text"];
-  $articleUuid = Uuid::uuid4();
-
   $servername = "localhost:8889";
   $username = "exercise_user2";
   $password = "ezFz_cnb1(*aeKV.";
   $database = "exercise_data";
   
   // Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
- $sql="INSERT INTO articles (uuid, title, type) VALUES ('$articleUuid', '$title', 'article')";
+  $conn = mysqli_connect($servername, $username, $password, $database);
+
+  // Prevent SQL injection
+ $title = mysqli_real_escape_string($conn,  $_POST["title"]);
+ $text = mysqli_real_escape_string($conn,  $_POST["text"]);
+$articleUuid = Uuid::uuid4();
+
+$sql="INSERT INTO articles (uuid, title, type) VALUES ('$articleUuid', '$title', 'article')";
 
  if (mysqli_query($conn, $sql)) {
   $sqlParagraph = "INSERT INTO paragraphs (article_id, text) VALUES ('$articleUuid', '$text')";
    if (mysqli_query($conn, $sqlParagraph)) {
      echo "New record created successfully";
+     header('Location: index.php');
     } else {
   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
